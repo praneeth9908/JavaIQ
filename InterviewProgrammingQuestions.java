783c0a793b32a9b9745e790635d94e743d95bff5
praneeth <praneethkumar9908@gmail.com>
2023-11-06 17:25:00 +0530

    Added sorting Problem

--- InterviewProgrammingQuestions.java
+++ InterviewProgrammingQuestions.java
@@ -25,13 +25,13 @@ class InterviewProgrammingQuestions{
 // Ex:-
 // Input:- 1 2 3 4 5 6 3
 // Output:- 6
-    public static void largestElementInArray(int arr[]){
+    public static int largestElementInArray(int arr[]){
 
         int max = 0;
         for(int i = 0; i < arr.length; i++){
             max = Math.max(max, arr[i]);
         }
-        System.out.println(max);
+        return max;
 
     }
 
@@ -138,6 +138,108 @@ class InterviewProgrammingQuestions{
         return "Anagrams";
     }
 
+// 8. Array Sorting:
+// Implement sorting algorithms like bubble sort, merge sort, or quicksort.
+// 
+// Bubble Sort:-
+    public static int[] bubbleSort(int[] arr){
+
+        int temp;
+        boolean flag;
+        for(int i = 0; i < arr.length-1; i++){
+            flag = true;
+            for(int j = 0; j < arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = false;
+                }
+            }
+            if(flag)return arr;
+        }
+        return arr;
+    }
+
+// Merge Sort
+    public static void mergeSort(int[] arr, int l, int r){
+        if(l < r){
+            int m;
+            m = l + (r-l)/2;
+
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+
+            merge(arr, l, r, m);
+        }
+    }
+
+
+    private static void merge(int[] arr, int l, int r,int m) {
+        int[] arr1 = new int[m - l + 1];
+        int[] arr2 = new int[r - m];
+
+        for(int i = 0; i < arr1.length; i++){
+            arr1[i] = arr[l + i];
+        }
+        for(int i = 0; i < arr2.length; i++){
+            arr2[i] = arr[m + 1 + i];
+        }
+        int i = 0, j = 0, k = l;
+        while (i < arr1.length && j < arr2.length) {
+            if(arr1[i] < arr2[j]){
+                arr[k] = arr1[i];
+                i++;
+            }
+            else{
+                arr[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < arr1.length){  
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while(j < arr2.length){  
+            arr[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
+
+// Quick Sort
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int l, int r){
+
+        int pivot = r;
+        int i = l-1;
+        for(int j = l; j < r; j++){
+            if(arr[j] < arr[pivot]){
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, pivot);
+        return i + 1;
+    }
+    public static void quickSort(int[] arr, int l, int r){
+
+        if(l < r){
+
+            int p = partition(arr, l, r);
+
+            quickSort(arr, l, p-1);
+            quickSort(arr, p+1, r);
+        }
+    }
 
     public static void printArray(int arr[]){
         for(int i = 0; i < arr.length; i++){
@@ -152,8 +254,8 @@ class InterviewProgrammingQuestions{
         // System.out.println(reverseString(str));
 
         // int arr[] = {1, 2, 3, 4, 5, 6, 3};
-        // int res[] = largestElementInArray(arr);
-        // printArray(res)
+        // int res = largestElementInArray(arr);
+        // System.out.println(res);
 
         // String s = "MadaM";
         // System.err.println(isPolindrome(s));
@@ -171,6 +273,21 @@ class InterviewProgrammingQuestions{
         // String s1 = "credit";
         // String s2 = "credit";
         // System.out.println(isAnagrams(s1, s2));
+
+        // int arr1[] = {3, 5, 1, 2, 3, 4, 5, 6, 3};
+        // int res1[] = bubbleSort(arr1);
+        // printArray(res1);
+
+        // int arr[] = {3, 5, 1, 2, 3, 4, 5, 6, 3};
+        // int l = 0, r = arr.length-1;
+        // mergeSort(arr, l, r);
+        // printArray(arr);
+
+        // int arr[] = {3, 5, 1, 8, 7, 2345, 9876, 45, 987, 0, 2, 3, 4, 5, 6, 3};
+        // int l = 0, r = arr.length-1;
+        // quickSort(arr, l, r);
+        // printArray(arr);
+
 
     }
 }
